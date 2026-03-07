5426c95378e640cd02ba7a604b5b95f45ab12199
hubertDgomes <gomeshubert8@gmail.com>
2026-03-08 05:23:36 +0600

    sd

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -3,40 +3,41 @@ import React, { useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-
 const Signup = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [name , setName] = useState("")
-  const [email , setEmail] = useState("")
-  const [password , setPassword] = useState("")
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const handleSign = (e) => {
-    e.preventDefault()
-    try{
-      console.log("Sending signup request...");
-      const res = axios.post("https://auth-backend-qvke.onrender.com/api/signup",{
-        name,
-        email,
-        password
-      },{withCredentials : true})
-      toast.success(`If ${email} is valid, then check your email inbox to verify the OTP.`);
-      navigate("/otp" , {state : {email}})
-    }
-    catch(err){
+  const handleSign = async (e) => {
+    e.preventDefault();
+    try {
+      const res = axios.post(
+        "https://auth-backend-qvke.onrender.com/api/signup",
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          withCredentials : true
+        }
+      );
+      toast.success(
+        `If ${email} is valid, then check your email inbox to verify the OTP.`,
+      );
+      navigate("/otp", { state: { email } });
+    } catch (err) {
       if (err.response) {
-        toast.error(err.response.data.msg)
+        toast.error(err.response.data.msg);
       } else if (err.request) {
-        toast.error("Server did not respond. Try again later.")
+        toast.error("Server did not respond. Try again later.");
       } else {
-        toast.error("Something went wrong!")
+        toast.error("Something went wrong!");
       }
-      
     }
-    
-  }
-
+  };
 
   return (
     <>
@@ -45,7 +46,7 @@ const Signup = () => {
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Enter your name</legend>
             <input
-              onChange={(e)=> setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               className="input"
               placeholder="Type your name"
@@ -54,7 +55,7 @@ const Signup = () => {
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Enter the email</legend>
             <input
-              onChange={(e)=> setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
               className="input"
               placeholder="Type email"
@@ -64,7 +65,7 @@ const Signup = () => {
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Enter the password?</legend>
             <input
-              onChange={(e)=> setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               type="password"
               className="input"
               placeholder="Type password"
